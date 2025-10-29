6b2b8b533a4c78f6823e993af2a7ecc00cf9c5bc
natalylodeiro <natalylodeiro@gmail.com>
2025-10-29 10:46:39 +0100

    Se agregó el conjunto de datos colegio

---
+++ R/colegio.R
@@ -0,0 +1,14 @@
+#' Datos del colegio Inem
+#'
+#' Este conjunto de datos pertenece a estudiantes de 6 grado del colegio Inem
+#'
+#' @format ## `colegio`
+#' A data frame with 3 rows and 3 columns:
+#' \describe{
+#'   \item{edad}{edad del estudiante en años}
+#'   \item{peso}{peso corporal en kg}
+#'   \item{gafas}{es TRUE si el estudiante usa gafas, caso contrario FALSE}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"colegio"
